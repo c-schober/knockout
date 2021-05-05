8e74a3882e0fc41d48695920687192483342dd1b
info@christoph-schober.com <info@christoph-schober.com>
2021-05-05 14:41:31 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
-class App {
-    constructor() {
-        this.name = "yo"
-    }
-    changeName = () => {
-        this.name = 'lo'
-    }
+const state = {
+  name: ko.observable('Yolo'),
 }
 
-const app = new App();
+class ViewModel1 {
+  constructor() {
+    this.name = state.name
+  }
+}
+
+class ViewModel2 {
+  constructor() {
+    this.fullName = ko.computed(() => {
+      return state.someProperty() + 'swag'
+    })
+    this.updateTheProperty = () => {
+      state.someProperty('lit')
+    }
+  }
+}
